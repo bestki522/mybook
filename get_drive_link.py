312942266a20fddb9c3759bb9e21bf0b002f71ce
ivan <co.tran@paradiseio.com>
2021-10-27 14:06:49 +0700

    update get_drive_link.py

--- get_drive_link.py
+++ get_drive_link.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def get_drive_link(page):
@@ -8,11 +9,12 @@ def get_drive_link(page):
     html_content = requests.get(page).text  # get html content in text format
     soup = BeautifulSoup(html_content, 'html.parser')  # parse html text content to bs4 object
     gd = str()
-    # get all <a> tag with <href not null> and text equal "PDF"
-    for h in soup.findAll('a', href=True, text="PDF"):
+    # get all <a> tag with <href not null> and text exact match "PDF" or "Here"
+    # for h in soup.findAll('a', href=True, text="PDF"):
+    for h in soup.findAll('a', href=True, text=re.compile(r'^PDF|^Here')):
         if h.text:
             gd = h['href']
     return gd
 
-
-#print(get_drive_link("https://chiasemoi.com/chien-luoc-cua-me-thay-doi-cuoc-doi-con.html"))
+#print(get_drive_link("https://chiasemoi.com/thuc-don-an-dam-kieu-nhat-cho-be-5-15-thang-tuoi.html"))
+#print(get_drive_link("https://chiasemoi.com/suc-manh-cua-viec-dat-cau-hoi-dung.html"))
